1f1f8bf10a3ea363b92765e190bdf26f293662e3
tofy-dev <tofyninja@gmail.com>
2022-06-20 17:07:55 -0700

    fix media player (automatically update images, remove delay)

--- eww/scripts/get_image/get_image.sh
+++ eww/scripts/get_image/get_image.sh
@@ -2,19 +2,24 @@ DIR="/home/$USER/.config/eww/scripts/get_image"
 image_link=`playerctl metadata | grep "artUrl" | awk '{print $3}'`
 
 # obtain image, store it in ./temp_img
-if [ "${image_link:0:5}" == "https" ]
+prev_img=`cat ${DIR}/prev_img.txt`
+if [ "${image_link}" != "${prev_img}" ]
+then
+    if [ "${image_link:0:5}" == "https" ]
     then
-        prev_img=`cat ${DIR}/prev_img.txt`
-        if [ "${image_link}" != "${prev_img}" ]
-        then
-            wget -O ${DIR}/img ${image_link}
-        fi
+        wget -O ${DIR}/img ${image_link}
     else
         length=`echo -n ${image_link} | wc -c`
         image_link=${image_link:7:length}
         cp ${image_link} ${DIR}/img
+        magick ${DIR}/img -gravity center -crop 150x150+0+0 +repage ${DIR}/img
+    fi
 fi
 
 # update last image
 rm ${DIR}/prev_img.txt
 echo ${image_link} >> ${DIR}/prev_img.txt;
+
+# modify path in eww
+eww update image-path="./scripts/get_image/"
+eww update image-path="./scripts/get_image/img"
